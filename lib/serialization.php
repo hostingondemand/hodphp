32e1bf8cd4167982640325c1422bb429f4a1561d
Mark Smit <m.smit@hostingondemand.nl>
2016-10-13 13:39:11 +0200

    CC-42 Added support for (error) messages.. prefixed annotations and some serialisation events

--- lib/serialization.php
+++ lib/serialization.php
@@ -17,12 +17,20 @@ class Serialization extends \core\Lib
                     $data[$key]=$data[$key]->toArray();
                 }
             }
+            $type ="array";
         }
         else if(is_object($data) && method_exists($data,"toArray")){
+            $type= get_class($data);
             $data=$data->toArray();
         }
-
-        return $this->LoadSerializer($format)->serialize($data);
+        $eventData=array(
+            "data"=>$data,
+            "type"=>$type,
+        );
+        $eventData=$this->event->raise("preSerialize",$eventData);
+        $eventData["data"]=$this->LoadSerializer($format)->serialize($eventData["data"]);
+        $eventData=$this->event->raise("postSerialize",$eventData);
+        return $eventData["data"];
     }
 
     function unserialize($format,$data,$assoc=false){
