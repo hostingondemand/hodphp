ad7413402684b2077ee87056c660d75f94300980
Alen Androsevic <alenandrosevic@gmail.com>
2016-12-16 15:51:03 +0100

    Added fieldHandlers for models & modellists

---
+++ provider/fieldHandler/model.php
@@ -0,0 +1,50 @@
+<?php
+namespace provider\fieldHandler;
+use lib\model\BaseFieldHandler;
+
+
+class Model extends BaseFieldHandler
+{
+    private $_class;
+    private $_namespace;
+    private $_data;
+
+    function fromAnnotation($parameters,$type,$field)
+    {
+        $this->_class = $parameters[0];
+        $this->_namespace = @$parameters[1] ?: false;
+    }
+
+    function model($class, $namespace = false) {
+        $this->_class = $class;
+        $this->_namespace = $namespace;
+    }
+
+    function get($inModel){
+        if(empty($this->_data)){
+            return null;
+        }
+        return $this->_data;
+    }
+
+    function set($value){
+        if(!$this->_namespace) {
+            $model = $this->model->{$this->_class};
+        } else {
+            $model = $this->model->{$this->_namespace}->{$this->_class};
+        }
+
+        if(is_object($value)){
+            $this->_data = $value;
+        } else if(is_array($value)) {
+            $this->_data = $model->fromArray($value);
+        } else if (method_exists($model, 'initialize')) {
+            $this->_data = $model->initialize($value);
+        } else {
+            $this->_data = null;
+        }
+
+        $this->_model->_setInData($this->_inModelName,$this->_data);
+    }
+
+}
