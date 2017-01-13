93613dc75f174f3f1e7e8957812e1256fc035977
Mark Smit <m.smit@hostingondemand.nl>
2017-01-13 17:12:20 +0100

    added currency support and some minor fixes

---
+++ provider/templateFunction/currency.php
@@ -0,0 +1,24 @@
+<?php
+namespace provider\templateFunction;
+
+class FuncCurrency extends \lib\template\AbstractFunction
+{
+    function call($parameters, $data, $content = "", $unparsed = Array(),$module=false)
+    {
+        $currency=$this->config->get("currency.symbol","website");
+
+        if(!$currency){
+            $currency="€";
+        }
+        if(is_numeric($parameters[0])){
+            $value=$parameters[0];
+        }else{
+            $value=0;
+        }
+        $formatted=number_format($value,2);
+
+        return $currency." ".$formatted;
+    }
+}
+
+?>
