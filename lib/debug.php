b5d5f7eff119fc5b58e4ef52ed4e93e201199baf
Mark Smit <m.smit@hostingondemand.nl>
2017-05-12 13:35:58 +0200

    CC-39 -- added logging

--- lib/debug.php
+++ lib/debug.php
@@ -1,22 +1,28 @@
 <?php
 namespace hodphp\lib;
+
 use hodphp\core\Lib;
 use hodphp\core\Loader;
 
 class Debug extends Lib
 {
 
-    var $errors = array();
+    var $messages = array();
     var $trace = array();
     var $profileStack = array();
     var $profiles = array();
 
+    var $levels = ["debug", "info", "error", "fatal"];
+
     function getInitArray()
     {
-        return array(
-            "errors" => $this->errors,
-            "profiles" => $this->removeKeys($this->profiles)
-        );
+        if ($this->session->_debugMode) {
+            return array(
+                "messages" => $this->messages,
+                "profiles" => $this->removeKeys($this->profiles)
+            );
+        }
+
     }
 
     function removeKeys($input)
@@ -36,21 +42,41 @@ class Debug extends Lib
         }
     }
 
-    function error($title, $detail)
+    function message($title, $detail, $level)
     {
         $trace = array_slice($this->trace, -5, 5, true);
-        $this->errors[] = array("title" => $title, "detail" => $detail, "stackTrace" => $trace);
+        $this->messages[] = array("title" => $title, "detail" => $detail, "stackTrace" => $trace, "level" => $level, "levelName" => $this->levels[$level]);
+    }
+
+    function fatal($title, $detail)
+    {
+        $this->message($title, $detail, 3);
+    }
+
+    function error($title, $detail)
+    {
+        $this->message($title, $detail, 2);
+    }
+
+    function info($title, $detail)
+    {
+        $this->message($title, $detail, 1);
+    }
+
+    function debug($title, $detail)
+    {
+        $this->message($title, $detail, 0);
     }
 
     function handleShutdown()
     {
         $error = error_get_last();
         if ($error['type'] === E_ERROR || $error['type'] == E_PARSE) {
-            $this->error("PHP:" . $error["message"], $error);
+            $this->fatal("PHP:" . $error["message"], $error);
             $this->response->setPartialMode(false);
             Loader::loadAction(array("fatalError", "home"));
+            $this->__destruct();
         }
-        //die();
     }
 
     function addToTrace($actionType, $class, $name, $arguments = array())
@@ -86,6 +112,32 @@ class Debug extends Lib
         $this->profiles[$last["name"]]["occurances"]++;
         $this->profiles[$last["name"]]["seconds"] += $time;
 
+    }
+
+    function __destruct()
+    {
+        $status = $this->config->get("debug.status", "server") ?: false;
+        if ($status) {
+            $result = "";
+            $level = $this->config->get("debug.level", "server") ?: 0;
+            $folder = $this->config->get("debug.folder", "server") ?: "data/log/";
+
+            foreach ($this->messages as $message) {
+                if ($message["level"] >= $level) {
+                    $result .= "--------------" . $message["title"] . "---------------\n";
+                    $result .= "Level:" . $message["levelName"] . " \n";
+                    $result .= "detail:" . print_r($message["detail"],true)."\n";
+
+                }
+            }
+
+            if ($result) {
+                $result = "\n==============" . date("d-m-Y H:i:s") . "==============\n" . $result;
+                $this->filesystem->mkdir($folder);
+                $this->filesystem->append($folder.date("Y-W").".log",$result);
+            }
+
+        }
     }
 
     function _debugIn($type, $name, $arguments = array())
