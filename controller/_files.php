7419b89f1c402f1e327326468b5d919435fa0818
Kim Hoogenberg <kimhoogenberg@Kims-MacBook-Pro.local>
2017-06-21 23:48:45 +0200

    SCM -- Some changes to serve proper css and html

--- controller/_files.php
+++ controller/_files.php
@@ -9,11 +9,17 @@ class _files extends Controller
         //load files
         ob_clean();
         $file = implode("/", func_get_args());
-        $content = $this->template->parse($this->filesystem->getFile("content/" . $file),[]);
 
         //set headers
         $contentType = $this->filesystem->getContentType("content/" . $file);
         $this->response->cache(900);
+
+        if ($contentType == "text/css") {
+            $content = $this->template->parse($this->filesystem->getFile("content/" . $file), []);
+        }
+        else {
+            $content = $this->filesystem->getFile("content/" . $file);
+        }
 
         //show content
         $this->response->renderFile($content, $contentType);
