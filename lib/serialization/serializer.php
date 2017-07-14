c65959670c65c04889132f294c043f66a7c6ca03
Mark Smit <m.smit@hostingondemand.nl>
2017-07-14 14:24:45 +0200

    serializer try catch

--- lib/serialization/serializer.php
+++ lib/serialization/serializer.php
@@ -10,103 +10,108 @@ abstract class Serializer extends \hodphp\core\Lib
 
     function prepareObject($data)
     {
-        $data = $this->getArrayData($data);
-        $newData = $data["data"];
-        if (is_array($data["type"])) {
-            foreach ($data["data"] as $key => $val) {
+        $original=$data;
+        try {
+            $data = $this->getArrayData($data);
+            $newData = $data["data"];
+            if (is_array($data["type"])) {
+                foreach ($data["data"] as $key => $val) {
 
-                if ($data["type"][$key] != "array" && $data["type"][$key] != "value") {
-                    $_data = array(
-                        "data" => $data["data"][$key],
-                        "type" => $data["type"][$key],
-                        "original" => $data["original"][$key]
-                    );
-                    $_dataNew = $this->prepareObject($_data);
-                    $newData[$key] = $_dataNew["data"];
-                    $newDataAnnotated[$key] = $_dataNew["annotated"];
-                } else {
-                    $newData[$key] = $val;
-                    $newDataAnnotated[$key] = array("annotations" => array(), "value" => $this->prepareObject($val));
-                }
-            }
-        }
-
-        if ($data["type"] != "array" && $data["type"] != "value") {
-            if (!is_array($data["type"])) {
-                foreach ($data["data"] as $key => $value) {
-                    $dkey = $key;
-                    $annotations = $this->annotation->getAnnotationsForField($data["type"], $key, "serialize");
-                    $translatedAnnotations = array();
-                    $valueAnnotated = array();
-                    $dynamicArray = false;
-                    foreach ($annotations as $annotation) {
-
-                        $annotation = $this->annotation->translate($annotation);
-                        $translatedAnnotations[$annotation->function] = $annotation;
-                        if ($annotation->function == "ignore") {
-                            unset($newData[$key]);
-                            unset($newDataAnnotated[$key]);
-                        } else {
-                            if ($annotation->function == "rename") {
-                                $tmp = $newData[$key];
-                                $tmpValueAnnotated = $valueAnnotated[$key];
-                                unset($newData[$key]);
-                                unset($newDataAnnotated[$key]);
-                                unset($valueAnnotated[$key]);
-                                $newData[$annotation->parameters[0]] = $tmp;
-                                $valueAnnotated[$key] = $tmpValueAnnotated;
-                                $key = $annotation->parameters[0];
-                            }
-                            if ($annotation->function == "dynamic") {
-                                $dynamicData = $this->dynamicGet($data, $dkey);
-                                $classAnnotations = array();
-                                if (!@$dynamicData["isNotArray"]) {
-                                    $dynamicArray = true;
-                                    if(is_array($dynamicData)) {
-                                        foreach ($dynamicData as $_key => $_value) {
-                                            $newData[$key][$_key] = $_value["data"];
-                                            $valueAnnotated[$key][$_key] = $_value["annotated"];
-                                            $classAnnotations[$_key] = $_value["classAnnotations"];
-                                        }
-                                    }
-                                } else {
-                                    $newData[$key] = $dynamicData["data"];
-                                    $valueAnnotated[$key] = $dynamicData["annotated"];
-                                    $classAnnotations = $dynamicData["classAnnotations"];
-                                }
-
-                            }
-                            if ($annotation->function == "enumName") {
-                                $newData[$key] = $newData[$key] = $data["original"]->{$key}->name;
-                            }
-                            if ($annotation->function == "modelOnly" && !property_exists(get_class($data["original"]), $key)) {
-                                unset($newData[$key]);
-                                unset($newDataAnnotated[$key]);
-                            }
-                        }
-                    }
-                    if ($dynamicArray) {
-                        $newDataAnnotated[$key]["_annotations"] = $translatedAnnotations;
-                        if(is_array($newData[$key])) {
-                            foreach ($newData[$key] as $_key => $_value) {
-                                $newDataAnnotated[$key][$_key] = array("_classAnnotations" => $classAnnotations[$_key], "_value" => $newData[$key][$_key]);
-                                if (isset($valueAnnotated[$key][$_key])) {
-                                    $newDataAnnotated[$key][$_key]["_annotated"] = $valueAnnotated[$key][$_key];
-                                }
-                            }
-                        }
+                    if ($data["type"][$key] != "array" && $data["type"][$key] != "value") {
+                        $_data = array(
+                            "data" => $data["data"][$key],
+                            "type" => $data["type"][$key],
+                            "original" => $data["original"][$key]
+                        );
+                        $_dataNew = $this->prepareObject($_data);
+                        $newData[$key] = $_dataNew["data"];
+                        $newDataAnnotated[$key] = $_dataNew["annotated"];
                     } else {
-                        $newDataAnnotated[$key] = array("_classAnnotations" => @$classAnnotations?:array(), "_annotations" => $translatedAnnotations, "_value" => $newData[$key]);
-                        if (isset($valueAnnotated[$key])) {
-                            $newDataAnnotated[$key]["_annotated"] = $valueAnnotated[$key];
-                        }
+                        $newData[$key] = $val;
+                        $newDataAnnotated[$key] = array("annotations" => array(), "value" => $this->prepareObject($val));
                     }
                 }
             }
-            $data["data"] = $newData;
-            $data["annotated"] = $newDataAnnotated;
+
+            if ($data["type"] != "array" && $data["type"] != "value") {
+                if (!is_array($data["type"])) {
+                    foreach ($data["data"] as $key => $value) {
+                        $dkey = $key;
+                        $annotations = $this->annotation->getAnnotationsForField($data["type"], $key, "serialize");
+                        $translatedAnnotations = array();
+                        $valueAnnotated = array();
+                        $dynamicArray = false;
+                        foreach ($annotations as $annotation) {
+
+                            $annotation = $this->annotation->translate($annotation);
+                            $translatedAnnotations[$annotation->function] = $annotation;
+                            if ($annotation->function == "ignore") {
+                                unset($newData[$key]);
+                                unset($newDataAnnotated[$key]);
+                            } else {
+                                if ($annotation->function == "rename") {
+                                    $tmp = $newData[$key];
+                                    $tmpValueAnnotated = $valueAnnotated[$key];
+                                    unset($newData[$key]);
+                                    unset($newDataAnnotated[$key]);
+                                    unset($valueAnnotated[$key]);
+                                    $newData[$annotation->parameters[0]] = $tmp;
+                                    $valueAnnotated[$key] = $tmpValueAnnotated;
+                                    $key = $annotation->parameters[0];
+                                }
+                                if ($annotation->function == "dynamic") {
+                                    $dynamicData = $this->dynamicGet($data, $dkey);
+                                    $classAnnotations = array();
+                                    if (!@$dynamicData["isNotArray"]) {
+                                        $dynamicArray = true;
+                                        if (is_array($dynamicData)) {
+                                            foreach ($dynamicData as $_key => $_value) {
+                                                $newData[$key][$_key] = $_value["data"];
+                                                $valueAnnotated[$key][$_key] = $_value["annotated"];
+                                                $classAnnotations[$_key] = $_value["classAnnotations"];
+                                            }
+                                        }
+                                    } else {
+                                        $newData[$key] = $dynamicData["data"];
+                                        $valueAnnotated[$key] = $dynamicData["annotated"];
+                                        $classAnnotations = $dynamicData["classAnnotations"];
+                                    }
+
+                                }
+                                if ($annotation->function == "enumName") {
+                                    $newData[$key] = $newData[$key] = $data["original"]->{$key}->name;
+                                }
+                                if ($annotation->function == "modelOnly" && !property_exists(get_class($data["original"]), $key)) {
+                                    unset($newData[$key]);
+                                    unset($newDataAnnotated[$key]);
+                                }
+                            }
+                        }
+                        if ($dynamicArray) {
+                            $newDataAnnotated[$key]["_annotations"] = $translatedAnnotations;
+                            if (is_array($newData[$key])) {
+                                foreach ($newData[$key] as $_key => $_value) {
+                                    $newDataAnnotated[$key][$_key] = array("_classAnnotations" => $classAnnotations[$_key], "_value" => $newData[$key][$_key]);
+                                    if (isset($valueAnnotated[$key][$_key])) {
+                                        $newDataAnnotated[$key][$_key]["_annotated"] = $valueAnnotated[$key][$_key];
+                                    }
+                                }
+                            }
+                        } else {
+                            $newDataAnnotated[$key] = array("_classAnnotations" => @$classAnnotations ?: array(), "_annotations" => $translatedAnnotations, "_value" => $newData[$key]);
+                            if (isset($valueAnnotated[$key])) {
+                                $newDataAnnotated[$key]["_annotated"] = $valueAnnotated[$key];
+                            }
+                        }
+                    }
+                }
+                $data["data"] = $newData;
+                $data["annotated"] = $newDataAnnotated;
+            }
+            return $data;
+        }catch (\Exception $ex){
+            return $original;
         }
-        return $data;
     }
 
     function getArrayData($data)
