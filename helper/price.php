5d3c2039e974e704cafb7be5dab9b095c93e5fac
Kim Hoogenberg <k.hoogenberg@comercia.nl>
2017-10-17 16:43:21 +0200

    CC-159 -- Let price helper support decimal seperator

--- helper/price.php
+++ helper/price.php
@@ -5,9 +5,10 @@ use hodphp\lib\helper\BaseHelper;
 
 class Price extends BaseHelper
 {
-    function value($price, $decimals = 2)
+    function value($price, $decimals = 2, $decimalSeperator = ".", $thousandsSeparator = "")
     {
         $rounded = round($price, $decimals, $this->config->get("currency.rounding", "website") ?: PHP_ROUND_HALF_EVEN);
-        return sprintf('%0.' . $decimals . 'f', $rounded);
+
+        return number_format($rounded, $decimals, $decimalSeperator, $thousandsSeparator);
     }
 }
