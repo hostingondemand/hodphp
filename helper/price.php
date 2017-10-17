55e43cac08f817e52545589a8c24e15225279c00
Kim Hoogenberg <k.hoogenberg@comercia.nl>
2017-10-17 21:59:06 +0200

    CC-159 -- Made setting more consistent with other settings

--- helper/price.php
+++ helper/price.php
@@ -5,10 +5,17 @@ use hodphp\lib\helper\BaseHelper;
 
 class Price extends BaseHelper
 {
-    function value($price, $decimals = 2, $decimalSeperator = ".", $thousandsSeparator = "")
+    function value($price, $decimals = 2, $decimalSeparator = false, $thousandsSeparator = false)
     {
         $rounded = round($price, $decimals, $this->config->get("currency.rounding", "website") ?: PHP_ROUND_HALF_EVEN);
 
-        return number_format($rounded, $decimals, $decimalSeperator, $thousandsSeparator);
+        if ( ! $decimalSeparator) {
+            $decimalSeparator = $this->config->get("number.decimal_separator", "website") ?: ".";
+        }
+        if ( ! $thousandsSeparator) {
+            $thousandsSeparator = $this->config->get("number.thousands_separator", "website") ?: "";
+        }
+
+        return number_format($rounded, $decimals, $decimalSeparator, $thousandsSeparator);
     }
 }
