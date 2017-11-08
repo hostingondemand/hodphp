3c48faad41c403b0a2ece747dc300c94f6909715
mark <m.smit@hostingondemand.nl>
2017-11-08 11:45:54 +0100

    upgrade dupdate dummy

--- modules/developer/model/console/updateDummy.php
+++ modules/developer/model/console/updateDummy.php
@@ -3,6 +3,7 @@
 namespace hodphp\modules\developer\model\console;
 
 use hodphp\core\Loader;
+use function hodphp\core\self;
 use hodphp\lib\model\BaseModel;
 use ReflectionMethod;
 
@@ -28,10 +29,11 @@ class UpdateDummy extends BaseModel
                 "extends"=>"Base"
             ],
         ];
+        ob_start();
         $this->getLibClasses();
         $this->getBaseClasses();
-        $this->getServices();
-
+        $this->fillDynamicClasses();
+        ob_clean();
         $result=$this->template->parseFile("console/dummyClasses",["classes"=>self::$classes]);
         if(!$this->filesystem->exists("data/dummyClasses")){
             $this->filesystem->mkdir("data/dummyClasses");
@@ -46,6 +48,12 @@ class UpdateDummy extends BaseModel
         $this->getDynamicProperties("framework/lib","hodphp\\lib",$result);
         $this->getDynamicProperties("project/lib","project\\lib",$result);
         return $result;
+    }
+
+    function fillDynamicClasses(){
+        $this->addFolderToClass("service","service","merge\\service","hodphp\\lib\\service",true,false);
+        $this->addFolderToClass("helper","helper","merge\\helper","hodphp\\lib\\helper",true,false);
+        $this->addFolderToClass("model","model","merge\\model","hodphp\\lib\\model",true,true);
     }
 
     function getBaseClasses(){
@@ -77,21 +85,53 @@ class UpdateDummy extends BaseModel
         return $result;
     }
 
-    function getServices(){
+    function addFolderToClass($folder, $classPath, $fakePath, $toClass, $modules=true, $addFoldersAsDummy=false){
         $properties=[];
-        $classes=$this->getClassesForDir("project/service","project\\service","merge\\service");
-        foreach($this->filesystem->getDirs("project/modules") as $dir){
-            $classes=array_merge($classes,
-                $this->getClassesForDir("project/modules/".$dir."/service","project\\modules\\".$dir."\\service","merge\\service")
-            );
+        $classes=$this->getClassesForDir("project/".$folder,"project\\".$classPath,$fakePath);
+
+        if($modules) {
+            foreach ($this->filesystem->getDirs("project/modules") as $dir) {
+                $classes = array_merge($classes,
+                    $this->getClassesForDir("project/modules/" . $dir . "/".$folder, "project\\modules\\" . $dir . "\\".str_replace("/","\\",$folder), $fakePath)
+                );
+            }
         }
 
         foreach($classes as $class){
-            $properties[]=["name"=>$class["name"],"cls"=>"\\".$class["namespace"]."\\".$class["name"]];
+            $properties[$class["name"]]=["name"=>$class["name"],"cls"=>"\\".$class["namespace"]."\\".$class["name"]];
         }
-        self::$classes["hodphp\\lib\\service"]["properties"]=$properties;
-    }
 
+        if($addFoldersAsDummy){
+
+            foreach ($this->filesystem->getDirs("project/model") as $dir) {
+                $dummyName=$fakePath . "\\dummyClass".$dir;
+                if(!isset(self::$classes[$dummyName])){
+                    self::$classes[$dummyName]=  ["namespace"=>$fakePath, "name"=>"dummyClass".$dir];
+                    $properties[$dir]=["name"=>$dir,"cls"=>"\\".$dummyName];
+                }
+                $this->addFolderToClass($folder."/".$dir,$classPath."\\". $dir ,$fakePath."\\dummyFolder".$dir,$dummyName,false,false);
+
+            }
+
+
+            if($modules){
+                foreach($this->filesystem->getDirs("project/modules") as $module){
+                    $modulePath="project/modules/".$module."/".$folder;
+                    foreach ($this->filesystem->getDirs($modulePath) as $dir) {
+                        $dummyName=$fakePath . "\\dummyClass".$dir;
+                        if(!isset(self::$classes[$dummyName])){
+                            $classes[$dummyName]=  ["namespace"=>$fakePath, "name"=>"dummyClass".$dir];
+                            $properties[$dir]=["name"=>$dir,"cls"=>"\\".$dummyName];
+                        }
+                        $this->addFolderToClass($modulePath,$classPath."\\". str_replace("/","\\",$modulePath) ,$fakePath."\\dummyFolder".$dir,$dummyName,false,false);
+
+                    }
+                }
+            }
+        }
+
+        self::$classes[$toClass]["properties"]=$properties;
+    }
 
     function getClassForFile($dir,$file,$classPath,$fakePath=false){
         $cls=str_replace(".php","",$file);
@@ -111,7 +151,7 @@ class UpdateDummy extends BaseModel
                     self::$classes[$fakePath . "\\" . $cls]["namespace"]=$fakePath;
                 } else {
                     self::$classes[$fakePath . "\\" . $cls]["properties"] = array_merge(self::$classes[$fakePath . "\\" . $cls]["properties"], self::$classes[$classPath . "\\" . $cls]["properties"]);
-                    self::$classes[$fakePath . "\\" . $cls]["methods"] = array_merge(self::$classes[$fakePath . "\\" . $cls]["methods"], self::$classes[$classPath . "\\" . $cls]["properties"]);
+                    self::$classes[$fakePath . "\\" . $cls]["methods"] = array_merge(self::$classes[$fakePath . "\\" . $cls]["methods"], self::$classes[$classPath . "\\" . $cls]["methods"]);
                 }
             }
 
