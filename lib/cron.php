e881cd45312bf6d25241e5ab92c09b487dcf07b3
mark <m.smit@hostingondemand.nl>
2018-03-25 01:14:30 +0100

    SCM-- minor bug fix with double interval and schedule

--- lib/cron.php
+++ lib/cron.php
@@ -15,14 +15,15 @@ class Cron extends \hodphp\core\Lib
         $info = Loader::getInfo($name, 'cron');
         $annotations = $this->annotation->getAnnotationsForClass($info->type, 'interval', true);
         $canRun = true;
-        if (!empty($annotations)) {
+        $useInterval=!empty($annotations);
+        if ($useInterval) {
             $translated = $this->annotation->translate($annotations[0]);
             $interval = $translated->parameters[0] * 60;
             $canRun = $this->provider->cronlog->default->needCronInterval($name, $interval);
         }
 
         $annotations = $this->annotation->getAnnotationsForClass($info->type, 'schedule', true);
-        if (!empty($annotations)) {
+        if (!empty($annotations) && (!$useInterval||!$canRun)) {
             $translated = $this->annotation->translate($annotations[0]);
             $schedule = $translated->parameters[0];
             $canRun = $this->provider->cronlog->default->needCronSchedule($name,$schedule);
