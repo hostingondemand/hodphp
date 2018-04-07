8cda77853f2748df7d3629bea10414c476203d37
mark <m.smit@hostingondemand.nl>
2018-04-07 23:52:12 +0200

    htmlspecialcahrs

--- provider/templateFunction/formatText.php
+++ provider/templateFunction/formatText.php
@@ -6,7 +6,7 @@ class FuncFormatText extends \hodphp\lib\template\AbstractFunction
     function call($parameters, $data, $content = "", $unparsed = Array(), $module = false)
     {
         //just returns the text
-        return nl2br($parameters[0]);
+        return nl2br(htmlspecialchars($parameters[0]));
     }
 }
 
