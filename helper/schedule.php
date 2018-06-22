6a9456dfbcf53b101606a8f3a925e57e9ac690b5
Mark Smit <m.smit@hostingondemand.nl>
2018-06-22 13:15:04 +0200

    CC-237

--- helper/schedule.php
+++ helper/schedule.php
@@ -6,41 +6,73 @@ use framework\lib\helper\BaseHelper;
 class Schedule extends BaseHelper
 {
 
-    function nextUpdate($schedule,$lastDone=false){
-        if(!$lastDone){
-            $lastDone=mktime(0,0,0,date("m"),date("d"),date("Y"));
+    function nextUpdate($schedule, $lastDone = 0, $pattern = "time")
+    {
+        if (method_exists($this, "nextUpdate_" . $pattern)) {
+            return call_user_func_array([$this, "nextUpdate_" . $pattern], func_get_args());
         }
-        $times=explode(" ",$schedule);
-        $lowestTime=0;
-        $bestTime=0;
-        foreach($times as $time){
-            $time=trim($time);
-            $exp=explode(":",$time);
-            $hour = $exp[0];
-            $minutes=$exp[1];
-            $timeToday=mktime($hour,$minutes,0,date("m"),date("d"),date("Y"));
+        return time();
+    }
 
-            if($timeToday<$lastDone && ($timeToday<$lowestTime||$lowestTime==0)){
-                $lowestTime=$timeToday;
-            }
-
-            if($timeToday>$lastDone && ($timeToday<$bestTime||$bestTime==0)){
-                $bestTime=$timeToday;
-            }
-
+    function nextUpdate_dayOfMonth($days, $lastDone)
+    {
+        if (!$lastDone) {
+            $lastDone = mktime(0, 0, 0, date("m"), 1, date("Y"));
         }
 
+        $days = explode(" ", $days);
+        $bestTime = 0;
+        foreach ($days as $day) {
+            $dayTime = mktime(0, 0, 1, date("m"), $day, date("Y"));
+
+            if ($dayTime >= $lastDone && ($dayTime < $bestTime || $bestTime == 0)){
+                $bestTime = $dayTime;
+            }
+        }
         if(!$bestTime){
-            $bestTime=$lowestTime+86400;
+            $bestTime=mktime(0, 0, 1, date("m")+1, min($days), date("Y"));
         }
 
         return $bestTime;
     }
 
-    function needUpdate($schedule, $lastDone)
+    function nextUpdate_time($schedule, $lastDone)
     {
-        $nextUpdate=$this->nextUpdate($schedule,$lastDone);
-        return $nextUpdate<time();
+        if (!$lastDone) {
+            $lastDone = mktime(0, 0, 0, date("m"), date("d"), date("Y"));
+        }
+
+        $times = explode(" ", $schedule);
+        $lowestTime = 0;
+        $bestTime = 0;
+        foreach ($times as $time) {
+            $time = trim($time);
+            $exp = explode(":", $time);
+            $hour = $exp[0];
+            $minutes = $exp[1];
+            $timeToday = mktime($hour, $minutes, 0, date("m"), date("d"), date("Y"));
+
+            if ($timeToday < $lastDone && ($timeToday < $lowestTime || $lowestTime == 0)) {
+                $lowestTime = $timeToday;
+            }
+
+            if ($timeToday > $lastDone && ($timeToday < $bestTime || $bestTime == 0)) {
+                $bestTime = $timeToday;
+            }
+
+        }
+
+        if (!$bestTime) {
+            $bestTime = $lowestTime + 86400;
+        }
+
+        return $bestTime;
+    }
+
+    function needUpdate($schedule, $lastDone, $pattern = "time")
+    {
+        $nextUpdate = $this->nextUpdate($schedule, $lastDone, $pattern = time());
+        return $nextUpdate < time();
     }
 }
 
